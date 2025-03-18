dff51e93fc3291fa4544e5cbd56be8a32c2f71fa
Petr Stepchenko <petr@statice.io>
2025-03-18 21:58:41 +0100

    add: integrate shadcn/ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -18,9 +21,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Button onClick={() => setCount((count) => count + 1)}>count id {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,50 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const isDev = command !== "build";
+  // if (isDev) {
+  //   // Terminate the watcher when Phoenix quits
+  //   process.stdin.on("close", () => {
+  //     process.exit(0);
+  //   });
+
+  //   process.stdin.resume();
+  // }
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    build: {
+      target: "esnext", // build for recent browsers
+      outDir: "../priv/static", // emit assets to priv/static
+      emptyOutDir: true,
+      sourcemap: isDev, // enable source map in dev build
+      manifest: false, // do not generate manifest.json
+      rollupOptions: {
+        input: {
+          main: "./src/main.ts",
+        },
+        output: {
+          entryFileNames: "frontend/[name].js", // remove hash
+          chunkFileNames: "frontend/[name].js",
+          assetFileNames: "frontend/[name][extname]",
+        },
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+        },
+      },
+    },
+  };
 })
